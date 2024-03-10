9e2a105c6e527c8f5937e1474e462417a63e5762
Marwan Aljawarneh <maljawarneh92@gmail.com>
2024-03-10 14:32:04 -0400

    week two

---
+++ practice_project/test_script.py
@@ -0,0 +1,20 @@
+import pytest
+from .script import is_prime
+
+
+def test_is_prime():
+     assert is_prime(17) is True
+     assert is_prime(13) is True
+     assert is_prime(11) is True
+     assert is_prime(7) is True
+     assert is_prime(5) is True
+
+
+def test_is_not_prime():
+     assert is_prime(4) is False
+     assert is_prime(6) is False
+     assert is_prime(8) is False
+     assert is_prime(10) is False
+     assert is_prime(12) is False
+     
+     
