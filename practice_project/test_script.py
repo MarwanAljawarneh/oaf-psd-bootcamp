8b12f50b87cab324bcf87a4db159a490b09adc74
Marwan Aljawarneh <maljawarneh92@gmail.com>
2024-03-11 15:55:59 -0400

    add more cases

--- practice_project/test_script.py
+++ practice_project/test_script.py
@@ -8,6 +8,8 @@ def test_is_prime():
      assert is_prime(11) is True
      assert is_prime(7) is True
      assert is_prime(5) is True
+     assert is_prime(107) is True
+     assert is_prime(207) is True
 
 
 def test_is_not_prime():
@@ -16,5 +18,7 @@ def test_is_not_prime():
      assert is_prime(8) is False
      assert is_prime(10) is False
      assert is_prime(12) is False
+     assert is_prime(108) is False
+     assert is_prime(206) is False
      
      
